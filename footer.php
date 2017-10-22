d3c3eaba2aa3b000c13912b3074301ff7ddf766e
Monica Sangam <ms792@njit.edu>
2017-10-22 16:02:46 -0400

    added footer file

---
+++ footer.php
@@ -0,0 +1,34 @@
+<html>
+<style>
+
+    #navigation_container {
+        margin: 0 auto;
+        width: 100%;
+    }
+
+    .rectangle {
+        background: #e5592e;
+        height: 62px;
+        position: relative;
+        -moz-box-shadow: 0px 0px 4px rgba(0,0,0,0.55);
+        box-shadow: 0px 0px 4px rgba(0,0,0,0.55);
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+        z-index: 500; /* the stack order: foreground */
+        margin: 3em 0;
+    }
+
+
+</style>
+<body>
+<div id="navigation_container">
+
+    <div class="rectangle">
+        <p >Created by Monica Sangam</p>
+        <p >Week 4 Project Assignment</p>
+    </div>
+</div>
+
+</body>
+</html>
